8480cad02f9c3ef3a3209fd86a7bb2354d837fb7
marcusphilips <marcellfulop2000@gmail.com>
2023-02-24 18:30:26 -0800

    Quality of Life Changes

--- space.hpp
+++ space.hpp
@@ -4,8 +4,8 @@
 #include "cell.hpp"
 #include "coordinate.hpp"
 /// @brief Space defines either the ship or the buffer. Now to make the math easier
-/// the top row is the 0th row. It is unoccupiable or else the crane would be blocked if
-/// it could be occupied by a container
+/// the top row is the index 0 row. That row is unoccupiable or else the crane would 
+/// be blocked if it could be occupied by a container
 class Space {
     private:
     Cell** cells;
